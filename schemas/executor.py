11eab10167ec8511c707dcca85d3499d9860fe86
Alexandr Andreev <1996sasha2507@mail.ru>
2025-10-12 16:56:50 +0300

    Fix some

--- schemas/executor.py
+++ schemas/executor.py
@@ -1,4 +1,25 @@
+from typing import List
+
 from pydantic import BaseModel
+
+from database.tables import Availability
+from schemas.profession import Job, Profession
 
 
 class ExecutorAdd(BaseModel):
+    tg_id: str
+    name: str
+    age: int
+    description: int
+    rate: str
+    experience: str
+    links: List[str]
+    tags: List[str]
+    availability: str = Availability.FREE
+    contacts: str | None
+    location: str | None
+    langs: List[str]
+    photo: bool
+    profession: Profession
+    jobs: List[Job]
+    verified: bool = False
