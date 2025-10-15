955aa95a1409afb9b2ce2e2b97b2316218b3fa55
Alexandr Andreev <1996sasha2507@mail.ru>
2025-10-15 22:54:04 +0300

    Changes

--- routers/messages/menu.py
+++ routers/messages/menu.py
@@ -5,4 +5,6 @@ def get_menu_message(role: str) -> str:
     """Сообщения для главного меню"""
     if role == UserRoles.CLIENT.value:
         return "Главное меню (Ваш текс)"
+    else:
+        return "Главное меню (Для исполнителя)"
 
